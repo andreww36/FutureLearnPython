1cc6f0ed536fd4d9da5248711dcf9ba829becc14
Andrew Watson <a.m.watson@ucl.ac.uk>
2021-12-27 17:50:45 +0000

    Playver lives added and randint (hit/miss) in player.attack()

--- GameApp.py
+++ GameApp.py
@@ -10,21 +10,35 @@ class Enemy:
     def __init__ (self):
         self.lives = lives
     def attack(self):
-        print("Ouch! That's a hit!")
-        self.lives = self.lives - 1
-        print(f'You have {self.lives} lives left')
-        return self.lives
+        while True:
+            play = input('Press return key to attack.')
+            if play == '':
+                break
+        r = random.randint(0,1)
+        if r == 1:
+            print("Ouch! That's a hit!")
+            self.lives = self.lives - 1
+            print(f'This enemy has {self.lives} lives left')
+            return self.lives
+        else:
+            print("Haha!  Missed!  Try again.")
     def checkLife(self):
         if self.lives == 0:
             print("This enemy is dead!")
+            return False
 
 # intro
-print('In this game you will confront three enemies, each with three lives.\nYour task is to eliminate each enemy to win the game.\n')
+import random
+print('''In this game you will confront three enemies, each with three lives.
+Your task is to eliminate each enemy to win the game.
+You will begin with 9 lives.  Each time you miss an enemy, you will lose a life.''')
+player_lives = 9
 for i in range(3):
     print('Here comes enemy number ' + str(i + 1))
     lives = Enemy.lives
     enemy = Enemy()
-    for i in range(3):
+    while True:
         enemy.attack()
+        player_lives -= 1
         enemy.checkLife()
 print("Well done, you have defeated three enemies.  Now take a break and relax!")
