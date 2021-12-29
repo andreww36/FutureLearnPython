c678787a828d1737751ad32eaf00546273bff74b
Andrew Watson <a.m.watson@ucl.ac.uk>
2021-12-29 09:02:33 +0000

    Player class added Enemy class has 'lives' error - not defined

--- GameApp.py
+++ GameApp.py
@@ -17,28 +17,50 @@ class Enemy:
         r = random.randint(0,1)
         if r == 1:
             print("Ouch! That's a hit!")
-            self.lives = self.lives - 1
+            self.lives -= 1
             print(f'This enemy has {self.lives} lives left')
-            return self.lives
         else:
             print("Haha!  Missed!  Try again.")
+            return -1
     def checkLife(self):
         if self.lives == 0:
             print("This enemy is dead!")
             return False
+
+class Player:
+    lives = 9
+    def __init__(self, name):
+        self.name = name
+    def hi_player(self):
+        print(f'Hi {self.name}!')
+    def checkLife(self):
+        self.lives -= 1
+        if self.lives == 0:
+            print(f"Hard luck {self.name}, you've lost all your lives.  Better luck next time!")
+            quit()
+        else:
+            print(f'You have {self.lives} lives left.')
 
 # intro
 import random
 print('''In this game you will confront three enemies, each with three lives.
 Your task is to eliminate each enemy to win the game.
 You will begin with 9 lives.  Each time you miss an enemy, you will lose a life.''')
-player_lives = 9
+player = Player(input("OK, so what's your name? "))
+player_lives = player.lives
+player.hi_player()
 for i in range(3):
     print('Here comes enemy number ' + str(i + 1))
-    lives = Enemy.lives
     enemy = Enemy()
-    while True:
+    lives = enemy.lives
+    play = True
+    while play == True:
         enemy.attack()
-        player_lives -= 1
-        enemy.checkLife()
+        if enemy.attack() == -1:
+            player_lives.checkLife()
+        else:
+            lives.checkLife()
+            if lives.checkLife() == False:
+                play = False
+
 print("Well done, you have defeated three enemies.  Now take a break and relax!")
