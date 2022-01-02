6c0c5cb58cb514154dd1a331bb48316de4e0fd3d
Andrew Watson <a.m.watson@ucl.ac.uk>
2022-01-02 23:03:53 +0000

    player_lives specified

--- GameApp.py
+++ GameApp.py
@@ -7,8 +7,6 @@
 # class Enemy
 class Enemy:
     lives = 3
-    def __init__ (self):
-        self.lives = lives
     def attack(self):
         while True:
             play = input('Press return key to attack.')
@@ -19,27 +17,27 @@ class Enemy:
             print("Ouch! That's a hit!")
             self.lives -= 1
             print(f'This enemy has {self.lives} lives left')
+            return 0
         else:
             print("Haha!  Missed!  Try again.")
-            return -1
     def checkLife(self):
         if self.lives == 0:
             print("This enemy is dead!")
             return False
 
 class Player:
-    lives = 9
+    player_lives = 9
     def __init__(self, name):
         self.name = name
     def hi_player(self):
         print(f'Hi {self.name}!')
-    def checkLife(self):
-        self.lives -= 1
+    def checkPlayerLife(self):
+        self.player_lives -= 1
         if self.lives == 0:
             print(f"Hard luck {self.name}, you've lost all your lives.  Better luck next time!")
             quit()
         else:
-            print(f'You have {self.lives} lives left.')
+            print(f'You have {self.player_lives} lives left.')
 
 # intro
 import random
@@ -47,17 +45,19 @@ print('''In this game you will confront three enemies, each with three lives.
 Your task is to eliminate each enemy to win the game.
 You will begin with 9 lives.  Each time you miss an enemy, you will lose a life.''')
 player = Player(input("OK, so what's your name? "))
-player_lives = player.lives
+player_lives = player.player_lives
+print(str(player_lives)) # check line
 player.hi_player()
 for i in range(3):
     print('Here comes enemy number ' + str(i + 1))
     enemy = Enemy()
     lives = enemy.lives
+    print(str(lives)) # check line
     play = True
     while play == True:
         enemy.attack()
-        if enemy.attack() == -1:
-            player_lives.checkLife()
+        if enemy.attack() != 0:
+            player_lives.checkPlayerLife()
         else:
             lives.checkLife()
             if lives.checkLife() == False:
