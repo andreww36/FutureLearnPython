f1f067a73a5f6c315c49dbce8b2ee4ca7bccc33a
Andrew Watson <a.m.watson@ucl.ac.uk>
2022-01-12 22:59:36 +0000

    Additional rooms

--- room_game.py
+++ room_game.py
@@ -5,18 +5,40 @@ from item import Item
 # create rooms
 kitchen = Room("Kitchen")
 ballroom = Room("Ballroom")
-dining_hall = Room("Dining Hall")
+dining_hall = Room("Dining hall")
+cellar = Room("Cellar")
+drawing_room = Room("Drawing room")
+pantry = Room("Pantry")
+hall = Room("Hall")
+bedroom = Room("Bedroom")
 
 # set descriptions
 kitchen.set_description("A dank and dirty room buzzing with flies.")
 ballroom.set_description("A grand room with chandeliers and a grandfather clock in the corner.")
 dining_hall.set_description("A cold room with a large table and 18 chairs covered with cobwebs.")
+cellar.set_description("A spooky, cavernous room crawling with spiders.")
+drawing_room.set_description("A cosy room with two sofas and a blazing fire.")
+pantry.set_description("A musty room containing bottles of silver polish and knife sharpeners.")
+hall.set_description("A galleried reception room with a grand central staircase.")
+bedroom.set_description("A large room with a four-poster bed and window overlooking the park.")
 
 # linking rooms together
 kitchen.link_room(dining_hall, "south")
 dining_hall.link_room(kitchen, "north")
 dining_hall.link_room(ballroom, "west")
 ballroom.link_room(dining_hall, "east")
+kitchen.link_room(cellar, "east")
+cellar.link_room(kitchen, "west")
+cellar.link_room(pantry, "south")
+pantry.link_room(cellar, "north")
+ballroom.link_room(drawing_room, "south")
+drawing_room.link_room(ballroom, "north")
+dining_hall.link_room(hall, "south")
+hall.link_room(dining_hall, "north")
+hall.link_room(bedroom, "east")
+bedroom.link_room(hall, "west")
+drawing_room.link_room(hall, "east")
+hall.link_room(drawing_room, "west")
 
 # creating enemy
 dave = Enemy("Dave", "A smelly zombie")
