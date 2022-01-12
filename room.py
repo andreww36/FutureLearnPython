04d3b30dca86ee8e9b04f64f8f81e5249eddc611
Andrew Watson <a.m.watson@ucl.ac.uk>
2022-01-12 22:13:04 +0000

    First draft

---
+++ room.py
@@ -0,0 +1,48 @@
+class Room():
+    def __init__(self, room_name):
+        self.name = room_name
+        self.description = None
+        self.linked_rooms = {}
+        self.character = None
+
+    def set_description(self, room_description):
+        self.description = room_description
+
+    def get_description(self):
+        return self.description
+
+    def set_name(self, room_name):
+        self.name = room_name
+
+    def get_name(self):
+        return self.name
+
+    def describe(self):
+        print(self.description)
+
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
+        # print( self.name + " linked rooms :" + repr(self.linked_rooms) )
+        # this creates the dictionary: linked_rooms = {direction: room_to_link}
+        # i.e. for this instance of Room, this dictionary is created
+
+    def get_details(self):
+        print(self.name)
+        print("-----------------")
+        print(self.description)
+        for direction in self.linked_rooms:
+            room = self.linked_rooms[direction]
+            print( "The " + room.get_name() + " is " + direction)
+
+    def move(self, direction):
+        if direction in self.linked_rooms:
+            return self.linked_rooms[direction]
+        else:
+            print("You can't go that way")
+            return self
+
+    def set_character(self, new_character):
+        self.character = new_character
+
+    def get_character(self):
+        return self.character
